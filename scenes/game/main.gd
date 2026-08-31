ceffc9837620b02b7c27d337bf276927f367afc1
Ryan James <102490+Rejam@users.noreply.github.com>
2026-08-31 22:10:20 +0100

    drop ball button manual button connections balls_per_round and balls_remaining variables

--- scenes/game/main.gd
+++ scenes/game/main.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
 @export var boards: Array[PackedScene] = []
-@export var balls_remaining := 5
+@export var balls_per_round := 5
 
-@onready var board_marker: Node2D = $BoardMarker
+@onready var board_marker: Node2D = %BoardMarker
+@onready var next_round_button: Button = %NextRoundButton
+@onready var drop_ball_button: Button = %DropBallButton
 
 var ballScene := preload("res://scenes/game/ball/ball.tscn")
 var game_round := 0
 var board: Node2D = null
-var remaining_ball_count := 0
+var balls_remaining := 0
 
 func _ready() -> void:
+	next_round_button.pressed.connect(_next_round)
+	drop_ball_button.pressed.connect(_dropball)
 	_load_board()
 	_reset_balls()
 	
@@ -22,33 +26,27 @@ func _get_board() -> Node:
 	return boards[board_index].instantiate()
 	
 func _load_board() -> void:
-	clear_current_board()
+	_clear_current_board()
 	board = _get_board()
 	if not board: return
 	board_marker.add_child(board)
 
-func clear_current_board() -> void:
+func _clear_current_board() -> void:
 	if not board: return
 	board.queue_free()
 
 func _reset_balls() -> void:
-	remaining_ball_count = balls_remaining
+	balls_remaining = balls_per_round
 	
 func _next_round() -> void:
 	game_round += 1
-	_reset_balls.call_deferred()
+	_reset_balls()
 	_load_board.call_deferred()
 
-func _on_next_round_pressed() -> void:
-	_next_round()
-	
-func _input(event: InputEvent) -> void:
-	if event.is_action_released("drop_ball"):
-		dropball()
-
-func dropball() -> void:
-	if not remaining_ball_count: return
+func _dropball() -> void:
+	if not balls_remaining: return
+	if not board: return
 	var ball : Ball = ballScene.instantiate()
 	ball.position = Vector2(randi_range(100, 1000), randi_range(20, 50))
-	board_marker.add_child(ball)
-	remaining_ball_count -= 1
+	board.add_child(ball)
+	balls_remaining -= 1
