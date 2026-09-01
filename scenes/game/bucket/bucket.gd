2a84b43e2ed482f5955259a5b195d4fff489140b
Ryan James <102490+Rejam@users.noreply.github.com>
2026-09-01 17:02:23 +0100

    walls for boards and buckets

--- scenes/game/bucket/bucket.gd
+++ scenes/game/bucket/bucket.gd
@@ -29,10 +29,12 @@ const HEIGHT := 64.0
 		text_colour = value
 		_refresh()
 		
-@onready var _collision: CollisionShape2D = %CollisionShape2D
+@onready var _bucket_collision: CollisionShape2D = %BucketCollision
 @onready var _rect: ColorRect = %ColorRect
 @onready var _label: Label = %Label
-
+@onready var _left_wall_collision: CollisionShape2D = %LeftWallCollision
+@onready var _right_wall_collision: CollisionShape2D = %RightWallCollision
+@onready var _base_collision: CollisionShape2D = %BaseCollision
 
 func _ready() -> void:
 	_refresh()
@@ -44,18 +46,35 @@ func _refresh() -> void:
 	if not is_node_ready():
 		return
 	var size := Vector2(width, HEIGHT)
-	print(size)
+	_set_bucket_collision_area(size)
+	_resize_rect(size)
+	_resize_label(size)
+	_position_walls(size)
+
+func _set_bucket_collision_area(size: Vector2) -> void:
 	var shape := RectangleShape2D.new()
 	shape.size = size
-	_collision.shape = shape
+	_bucket_collision.shape = shape
+
+func _resize_rect(size: Vector2) -> void:
 	_rect.size = size
 	_rect.position = -size / 2.0
 	_rect.color = background_colour
+	
+func _resize_label(size: Vector2) -> void:
 	_label.size = size
 	_label.add_theme_color_override("font_color", text_colour)
 	_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 	_label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
-	_label.text = str(base_value)
+	_label.text = str(base_value)	
+
+func _position_walls(size: Vector2) -> void:
+	_left_wall_collision.shape.size = Vector2(1.0, size.y)
+	_left_wall_collision.position = Vector2(- size.x / 2, 0)
+	_right_wall_collision.shape.size = Vector2(1.0, size.y)
+	_right_wall_collision.position = Vector2( size.x / 2, 0)
+	_base_collision.shape.size = Vector2(size.x, 32.0)
+	_base_collision.position = Vector2(0, size.y / 2 + 16)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Ball:
